95679ca39c9effcf1ad8b9664ac9d8b8e9a84469
Matt Campbell <matt@mattcampbell.net>
2015-09-07 12:22:36 -0400

    Add demo and render to screen

---
+++ src/js/demo/huerizon.js
@@ -0,0 +1,23 @@
+export default class Huerizon {
+  constructor(screen) {
+    this.screen = screen;
+  }
+
+  update(time) {
+    let x, y;
+    let tx = Math.sin(time * 0.0002) * 20;
+    let ty = Math.cos(time * 0.0002) * -20;
+
+    for (y = 0; y < this.screen.height; y += 1) {
+      for (x = 0; x < this.screen.width; x += 1) {
+        this.screen.setPixel(
+          x,
+          y,
+          128 + Math.sin(time * 0.00125 + ((tx + x - this.screen.width / 2) / ((ty + y) - this.screen.height / 2))) * 128,
+          128 + Math.sin(-time * 0.001 + ((tx + x - this.screen.width / 2) / ((ty + y) - this.screen.height / 2))) * 128,
+          128 + Math.sin(-time * 0.0015 + ((tx + x - this.screen.width / 2) / ((ty + y) - this.screen.height / 2))) * 128
+        );
+      }
+    }
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log('hi');
+import Screen from 'screen';
+import Huerizon from 'demo/huerizon';
+
+let screen = new Screen(128, 96);
+screen.setScale(3);
+screen.setPadding(3);
+screen.appendTo(document.body);
+
+let demo = new Huerizon(screen);
+
+var update = function() {
+  requestAnimationFrame(update);
+  demo.update(Date.now());
+  screen.render();
+}
+
+update();
