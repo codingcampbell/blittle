11811618948bf3f9203dda17b0e69cd1295b88ea
Matt Campbell <matt@mattcampbell.net>
2015-09-13 21:22:13 -0400

    Might as well support all flexbox browsers

--- src/style/styles.js
+++ src/style/styles.js
@@ -1,3 +1,27 @@
+var flex = function(value) {
+  var prefixes = ['-webkit-', '-ms-', '']
+
+  return key => {
+    var result = {};
+    prefixes.forEach(prefix => {
+      var k = prefix + key;
+      k = k.replace(/-ms-align-items/, '-ms-flex-align');
+      k = k.replace(/-ms-justify-content/, '-ms-flex-pack');
+      result[k] = value;
+    });
+
+    return result;
+  };
+};
+
+var displayFlex = function() {
+  return () => ({
+    '  display': '-ms-flexbox',
+    ' display': '-webkit-flex',
+    display: 'flex',
+  });
+};
+
 module.exports = (async, util) => async({
   html: {
     boxSizing: 'border-box',
@@ -9,8 +33,6 @@ module.exports = (async, util) => async({
   },
 
   'html, body': {
-    height: '100%',
-    overflow: 'hidden',
     padding: 0,
     margin: 0
   },
@@ -19,10 +41,11 @@ module.exports = (async, util) => async({
     background: '#fff',
     fontFamily: 'Orbitron, helvetica, arial',
     fontSize: '2.4rem',
-    display: 'flex',
-    'flex-direction': 'column',
-    'justify-content': 'center',
-    'align-items': 'center'
+    display: displayFlex(),
+    'flex-direction': flex('column'),
+    'justify-content': flex('center'),
+    'align-items': flex('center'),
+    'padding': '25px 0'
   },
 
   h1: {
@@ -33,27 +56,29 @@ module.exports = (async, util) => async({
 
   '#content': {
     background: '#000',
-    display: 'flex',
-    'flex-direction': 'row',
-    'justify-content': 'center',
-    'align-items': 'center'
+    display: displayFlex(),
+    'flex-direction': flex('row'),
+    'justify-content': flex('center'),
+    'align-items': flex('center')
   },
 
   '#canvas-container': {
-    background: '#000',
+    background: '#000'
   },
 
   '#thumbnails': {
     margin: '5px',
-    display: 'flex',
-    'flex-direction': 'column',
+    display: displayFlex(),
+    'flex-direction': flex('column'),
 
     canvas: {
       cursor: 'pointer',
       margin: '10px',
+      '-webkit-transition': '-webkit-transform 0.2s ease',
       transition: 'transform 0.2s ease',
 
       '&:hover': {
+        '-webkit-transform': 'scale3d(1.1, 1.1, 1)',
         transform: 'scale3d(1.1, 1.1, 1)'
       }
     }
